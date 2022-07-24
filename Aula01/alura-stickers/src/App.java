a87620858e6b3347fd3ecde9ba30130302a29696
Fabio Louzada Colodette <fabio.loutte@gmail.com>
2022-07-24 20:19:06 -0300

    Realizando o desafio da Aula 01

--- Aula01/alura-stickers/src/App.java
+++ Aula01/alura-stickers/src/App.java
@@ -5,6 +5,7 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -23,12 +24,30 @@ public class App {
         System.out.println(listaDeFilmes.get(0));
 
         // exibir e manipular os dados
-        for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+        int pos = 0;
+        for (Map<String, String> filme : listaDeFilmes) {
+            pos++;
+            System.out.println("["+ pos +"]-------------------------------------------------------------------");
+            System.out.println("\u001b[1mTitulo:\u001b[m\t" + filme.get("title"));
+            System.out.println("\u001b[1mPoster:\u001b[m\t" + filme.get("image"));
+            System.out.println("\u001b[1mNota:\u001b[m \t" + filme.get("imDbRating"));
+            String nota = "";
+            for (int i = 0; i < (int) Float.parseFloat(filme.get("imDbRating")); i++) {
+                nota += "* ";
+            }
+            System.out.println("\u001b[37m\u001b[44m " + nota +" \u001b[m");
             System.out.println();
-            
+
         }
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("\u001b[1mDigite o número do filme que deseja ver:\u001b[m");
+        Scanner scanner = new Scanner(System.in);
+        int numero = scanner.nextInt();
+        System.out.println("--------------------------------------------");
+        System.out.println("\u001b[1mTitulo:\u001b[m\t\t1" + listaDeFilmes.get(numero-1).get("title"));
+        System.out.println("\u001b[1mNota Atual:\u001b[m\t" + listaDeFilmes.get(numero-1).get("imDbRating"));
+        System.out.println("De a sua propia avaliação para o filme: ");
+        float newNota = scanner.nextFloat();
+
     }
 }
